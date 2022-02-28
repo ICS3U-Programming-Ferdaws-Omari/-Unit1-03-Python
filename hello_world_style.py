8336ffe7ca6981650289b71327bcbfaae4a3f5d4
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-02-28 13:30:30 +0000

    I added a style cheker

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Created By: Ferdaws
+
+# Date: 02/24/2022
+
+# It is the hello world program with style.
+
+def main():
+    print("Hello world!")
+    
+    
+if __name__ == "__main__": 
+    main()
